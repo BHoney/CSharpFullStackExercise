a7ab70afdb530c98fc4e376a8f7d1188db435dca
Robert Moore <rob.b.moore89@gmail.com>
2025-01-28 18:45:58 -0500

    Initial Commit

---
+++ CSharpFullStackProject/Models/Meeting.cs
@@ -0,0 +1,54 @@
+﻿namespace CSharpFullStackProject.Models;
+
+public record Meeting
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
+
+public static class MeetingDB
+{
+    private static List<Meeting> _meetings = new List<Meeting>()
+    {
+        new Meeting
+        {
+            Id = 0, Name = "Meetings and You!", Description = "A meeting about meetings", StartTime = DateTime.Now,
+            EndTime = DateTime.Now.AddMinutes(30)
+        },
+        new Meeting
+        {
+            Id = 1, Name = "Standup - Engineering", Description = "Engineering team standup", StartTime = DateTime.Now,
+            EndTime = DateTime.Now.AddMinutes(30)
+        },
+        new Meeting
+        {
+            Id = 2, Name = "All Hands", Description = "We're all having a meeting now", StartTime = DateTime.Now,
+            EndTime = DateTime.Now.AddMinutes(30)
+        },
+    };
+    
+    public static List<Meeting> GetMeetings() => _meetings;
+    public static Meeting GetMeetingById(int id) => _meetings.FirstOrDefault(m => m.Id == id);
+    public static void AddMeeting(Meeting meeting) => _meetings.Add(meeting);
+
+    public static Meeting UpdateMeeting(Meeting update)
+    {
+        _meetings = _meetings.Select(meeting =>
+        {
+            if (meeting.Id == update.Id)
+            {
+                meeting.Name = update.Name;
+                meeting.Description = update.Description;
+                meeting.StartTime = update.StartTime;
+                meeting.EndTime = update.EndTime;
+            }
+            return meeting;
+        }).ToList();
+        return update;
+    }
+    
+    public static void DeleteMeeting(int id) => _meetings = _meetings.Where(room => room.Id != id).ToList();
+}
---
+++ CSharpFullStackProject/Routes/Meetings.cs
@@ -0,0 +1,16 @@
+﻿using static CSharpFullStackProject.Models.MeetingDB;
+
+namespace CSharpFullStackProject.Routes;
+
+public static class Meetings
+{
+    public static RouteGroupBuilder MapMeetingRoutesApi(this RouteGroupBuilder group)
+    {
+        group.MapGet("/", GetMeetings);
+        group.MapGet("/{id}", GetMeetingById);
+        group.MapPost("/{id}", AddMeeting);
+        group.MapPut("/", UpdateMeeting);
+        group.MapDelete("/{id}", DeleteMeeting);
+        return group;
+    }
+}
